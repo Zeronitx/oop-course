71059be8d1c8d127fed8743e111448046082ed03
Zeronit <lubomirm802@gmail.com>
2026-09-20 22:32:00 +0300

    Lab02 Task06

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,2 +1,2 @@
 ﻿System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-Task5.Run();
+Task6.Run();
---
+++ Lab02/Task6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public static class Task6
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        int[][] costs = new int[n][];
+        int maxIncome = 0, bestDoctorIdx = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int k = int.Parse(Console.ReadLine()!);
+            costs[i] = new int[k];
+            int sum = 0;
+            for (int j = 0; j < k; j++)
+            {
+                costs[i][j] = int.Parse(Console.ReadLine()!);
+                sum += costs[i][j];
+            }
+            double avg = (double)sum / k;
+            Console.WriteLine($"Doctor {i + 1}: {k} appointments, sum={sum} UAH, average={avg:F2} UAH");
+            if (sum > maxIncome)
+            {
+                maxIncome = sum;
+                bestDoctorIdx = i;
+            }
+        }
+        Console.WriteLine($"Highest income: Doctor {bestDoctorIdx + 1} ({maxIncome} UAH)");
+    }
+}
