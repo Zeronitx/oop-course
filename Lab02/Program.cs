190e78aa74544cc7bde4d1a9626abd80567581de
Zeronit <lubomirm802@gmail.com>
2026-09-20 22:07:16 +0300

    Lab02 Task02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,2 +1,2 @@
 ﻿System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-Task1.Run();
+Task2.Run();
---
+++ Lab02/Task2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public static class Task2
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        int[] queue = new int[n];
+        for (int i = 0; i < n; i++) queue[i] = int.Parse(Console.ReadLine()!);
+        string originalQueue = string.Join(" ", queue);
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (queue[j] > queue[j + 1])
+                {
+                    (queue[j], queue[j + 1]) = (queue[j + 1], queue[j]);
+                }
+            }
+        }
+        Console.WriteLine($"Queue (before): {originalQueue}");
+        Console.WriteLine($"Queue (after):  {string.Join(" ", queue)}");
+        Console.WriteLine($"Cheapest:       {queue[0]} UAH");
+        Console.WriteLine($"Most expensive: {queue[n - 1]} UAH");
+    }
+}
