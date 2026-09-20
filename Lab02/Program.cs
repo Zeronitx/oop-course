614e2165df18f5229386c061bd21e7ef983426f2
Zeronit <lubomirm802@gmail.com>
2026-09-20 22:15:25 +0300

    Lab02 Task03

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,2 +1,2 @@
 ﻿System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-Task2.Run();
+Task3.Run();
---
+++ Lab02/Task3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public static class Task3
+{
+    public static void Run()
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        int[] counts = new int[7];
+        int total = 0, maxIdx = 0, minIdx = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            counts[i] = int.Parse(Console.ReadLine()!);
+            total += counts[i];
+            if (counts[i] > counts[maxIdx]) maxIdx = i;
+            if (counts[i] < counts[minIdx]) minIdx = i;
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine($"{days[i],-12}: {counts[i]} patients");
+        }
+        Console.WriteLine($"Total:        {total}");
+        Console.WriteLine($"Most:         {days[maxIdx]} ({counts[maxIdx]})");
+        Console.WriteLine($"Least:        {days[minIdx]} ({counts[minIdx]})");
+    }
+}
