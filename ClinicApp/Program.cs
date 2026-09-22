ae86ed1973f0b1369ddff1780d7dcee74e3fb3e1
Zeronit <lubomirm802@gmail.com>
2026-09-22 15:50:05 +0300

    Lab03 Task05

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -6,38 +6,21 @@ class Program
 {
     static void Main()
     {
-        DoctorManager manager = new DoctorManager();
+        Appointment a1 = new Appointment(1, 1, new DateTime(2026, 5, 9, 10, 0, 0));
+        Appointment a2 = new Appointment(2, 2, new DateTime(2026, 5, 9, 11, 0, 0), 45);
+        Appointment a3 = new Appointment(3, 3, new DateTime(2026, 5, 10, 9, 0, 0), 20);
 
-        Doctor d1 = new Doctor("Oleh", "Sydorenko", "Cardiology", "LIC-001", "0441234567");
-        d1.WorkEndHour = 16;
-        Doctor d2 = new Doctor("Natalia", "Moroz", "Neurology", "LIC-002", "0442345678");
-        d2.WorkStartHour = 9;
-        d2.WorkEndHour = 18;
-        Doctor d3 = new Doctor("Andriy", "Vlasenko", "Pediatrics");
-        Doctor d4 = new Doctor("Anna", "Koval", "Cardiology", "LIC-004", "0445556677");
+        Console.WriteLine(a1.ToString());
+        Console.WriteLine(a2.ToString());
+        Console.WriteLine(a3.ToString());
 
-        manager.Add(d1);
-        manager.Add(d2);
-        manager.Add(d3);
-        manager.Add(d4);
+        Console.WriteLine("\nAfter Cancel and Complete:");
 
-        DoctorsMenu(manager);
-    }
+        a1.Cancel("Patient could not attend");
+        a2.Complete();
 
-    static void DoctorsMenu(DoctorManager manager)
-    {
-        manager.DisplayAll();
-        manager.DisplayStats();
-
-        Console.WriteLine("\nSearching for 'Cardiology':");
-        Doctor[] found = manager.FindBySpeciality("Cardiology");
-        foreach (var d in found)
-        {
-            Console.WriteLine(d.ToString());
-        }
-
-        Console.WriteLine("\nRemoving doctor with ID 4...");
-        manager.Remove(4);
-        manager.DisplayStats();
+        Console.WriteLine(a1.ToString());
+        Console.WriteLine(a2.ToString());
+        Console.WriteLine(a3.ToString());
     }
 }
