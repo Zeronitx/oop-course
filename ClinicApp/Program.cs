f94203b863ce3610698d469887d4a71512aa8a33
Zeronit <lubomirm802@gmail.com>
2026-09-22 15:41:40 +0300

    Lab03 Task02

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -6,16 +6,17 @@ class Program
 {
     static void Main()
     {
-        Patient p1 = new Patient("Ivan", "Petrenko", new DateTime(1985, 5, 10), "A+", "0501234567");
-        Patient p2 = new Patient("Olena", "Koval", new DateTime(1993, 8, 22), "B-", "0672345678");
-        Patient p3 = new Patient("Maksym", "Boiko", new DateTime(2010, 1, 15), "O+", "0933456789");
-        Patient p4 = new Patient("Unknown", "Patient", new DateTime(2000, 1, 1), "Unknown", "0000000000");
-        Patient p5 = new Patient("Maria", "Tkach");
+        Doctor d1 = new Doctor("Oleh", "Sydorenko", "Cardiology", "LIC-001", "0441234567");
+        d1.WorkEndHour = 16;
 
-        Console.WriteLine(p1.ToString());
-        Console.WriteLine(p2.ToString());
-        Console.WriteLine(p3.ToString());
-        Console.WriteLine(p4.ToString());
-        Console.WriteLine(p5.ToString());
+        Doctor d2 = new Doctor("Natalia", "Moroz", "Neurology", "LIC-002", "0442345678");
+        d2.WorkStartHour = 9;
+        d2.WorkEndHour = 18;
+
+        Doctor d3 = new Doctor("Andriy", "Vlasenko", "Pediatrics");
+
+        Console.WriteLine(d1.ToString());
+        Console.WriteLine(d2.ToString());
+        Console.WriteLine(d3.ToString());
     }
 }
